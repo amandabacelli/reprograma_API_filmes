4dfce1a757e36a22b8e4cae4855986a2910d4bc5
amandabacelli <amanda.sted@gmail.com>
2019-11-01 19:00:57 -0300

    att

--- src/routers/filmesRouter.js
+++ src/routers/filmesRouter.js
@@ -5,6 +5,6 @@ const controller = require('../controllers/filmesController')
 
 router.get('/', controller.get)
 router.get('/:diretor', controller.getDiretor)
-router.get('/:genero', controller.getGenero)
+router.get('/genero/:genre', controller.getGenero)
 
 module.exports = router
